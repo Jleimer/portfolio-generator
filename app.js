89b71c8af8cd1efcfb539f7eb6bc02bc0c90e1cc
Jonathan <eimerarts@gmail.com>
2020-07-25 14:32:50 -0500

    lesson 5 - unfinished

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
-const fs = require('fs');
+const { writeFile, copyFile } = require('./utils/generate-site.js');
 const inquirer = require('inquirer');
 const generatePage = require('./src/page-template.js');
+// const copyFile = () => {
+//   return new Promise((resolve, reject) => {
+//     fs.copyFile('./dist/index.html', fileContent, err => {
+//       // if there's an error, reject the Promise and send the error to the Promise's `.catch()` method
+//       if (err) {
+//         reject(err);
+//         // return out of the function here to make sure the Promise doesn't accidentally execute the resolve() function as well
+//         return;
+//       }
 
+//       // if everything went well, resolve the Promise and send the successful data to the `.then()` method
+//       resolve({
+//         ok: true,
+//         message: 'File copied!'
+//       });
+//     });
+//   });
+// };
 const templateData = 
   [{
     name: 'Lernantino',
@@ -165,16 +182,25 @@ if (!portfolioData.projects) {
 };
 const pageHTML = generatePage(templateData[0]);
 
+
 promptUser()
-//   .then(promptProject)
-//   .then(portfolioData => {
-//      const pageHTML = generatePage(portfolioData);
-
-    fs.writeFile('./index.html', pageHTML, err => {
-      if (err) throw new Error(err);
-
-    });
-  
+  .then(promptProject)
+  .then(portfolioData => {
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
+  });
 //const fs = require('fs');
 
 
